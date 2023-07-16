0d24c790f6faeb68ff61bf26d226a49800640703
Bonolo Cynthia Selemale <139655743+CynthiaSelemale@users.noreply.github.com>
2023-07-16 14:28:17 +0200

    Add files via upload

---
+++ NameSurnameApp.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package namesurnameapp;
+
+import za.ac.tut.ui.NameSurnameFrame;
+
+/**
+ *
+ * @author cynthia
+ */
+public class NameSurnameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NameSurnameFrame myFrame;
+        myFrame = new NameSurnameFrame();
+    }
+    
+}
---
+++ NameSurnameFrame.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author cynthia
+ */
+public class NameSurnameFrame extends JFrame{
+    public NameSurnameFrame(){
+        setTitle("Bonolo Cynthia Selemale");
+        setSize(200,300);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.GREEN);
+        setVisible(true);
+    }
+    
+}
